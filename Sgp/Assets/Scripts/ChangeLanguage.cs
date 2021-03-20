734ea06bed98c8bcd5744a8e4ecd3336f150818c
shruti2710 <shrutisnoomly@gmail.com>
2021-03-20 22:26:03 +0530

    Pushing converting word eng to guj

---
+++ Sgp/Assets/Scripts/ChangeLanguage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLanguage : MonoBehaviour
+{
+    public void ETG()
+    {
+        Lean.Localization.LeanLocalization.CurrentLanguage = "Gujarati";
+    }
+
+    public void GTE()
+    {
+        Lean.Localization.LeanLocalization.CurrentLanguage = "English";
+    }
+
+    
+
+}
